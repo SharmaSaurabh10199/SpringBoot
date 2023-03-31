34f7b15ef54eeea527fb17fdd47eadd8f9222f49
Sharma <sharmasaurabh10199@gmail.com>
2023-03-31 20:36:15 +0530

    tried injecting in application class, still bike is null

--- src/main/java/com/saurabh/practisespringconfig/PractiseSpringConfigApplication.java
+++ src/main/java/com/saurabh/practisespringconfig/PractiseSpringConfigApplication.java
@@ -1,13 +1,17 @@
 package com.saurabh.practisespringconfig;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PractiseSpringConfigApplication {
+	@Autowired
+	static Bike bike;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PractiseSpringConfigApplication.class, args);
+		bike.run();
 
 	}
 
